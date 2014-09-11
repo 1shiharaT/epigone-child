831e301a2d79b30347bddd4aae8168c1d68b3e4e
1shiharaT <akeome1369@gmail.com>
2014-09-12 00:34:13 +0900

    Create Blog Theme.

---
+++ modules/navbar.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Navbar module for child theme.
+ * =====================================================
+ * @package  epigone
+ * @license  GPLv2 or later
+ * @since 1.0.0
+ * =====================================================
+ */
+?>
+	<nav class="navbar navbar--default" role="navigation">
+		<div class="container">
+
+			<a href="<?php echo home_url(); ?>" class="navbar-symbol">
+				<i class="fa fa-home"></i>
+			</a>
+
+			<?php
+			/**
+			 * Global Navigation
+			 */
+				wp_nav_menu(
+					array(
+						'menu'              => 'primary',
+						'theme_location'    => 'primary',
+						'depth'             => 2,
+						'container'         => 'div',
+						'container_class'   => 'collapse cf navbar-collapse',
+						'container_id'      => 'header-navbar-collapse',
+						'menu_class'        => 'nav navbar-nav',
+						'fallback_cb'       => 'Epigone_Nav_Walker::fallback',
+						'walker'            => new Epigone_Nav_Walker(),
+					)
+				);
+			?>
+
+		</div>
+	</nav>
